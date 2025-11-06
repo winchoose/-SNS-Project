344529c2dc13a3d9f30de03566749988d57592a6
winchoose <tmdxor741@naver.com>
2025-11-06 16:32:08 +0900

    tanstack Query(데이터 조회 요청 관리하기)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCluent = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryCluent}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,14 +1,17 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodoData } from "@/hooks/quries/use-todos.data";
+import { API_URL } from "@/lib/constants";
+import type { Todo } from "@/store/types";
+import { useQuery } from "@tanstack/react-query";
 
 export default function TodoListPage() {
-  const todos = useTodos();
+  const { data: todos, isLoading, error } = useTodoData();
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">Todo list</h1>
       <TodoEditor />
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
