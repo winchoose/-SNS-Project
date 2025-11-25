33c60c90f57ab4e9aac2fb97e17965435d5a3a15
winchoose <tmdxor741@naver.com>
2025-11-25 11:25:37 +0900

    한입 로그 회원가입 구현

--- onebite-log/src/pages/sign-up-page.tsx
+++ onebite-log/src/pages/sign-up-page.tsx
@@ -1,17 +1,46 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useSignUp } from "@/hooks/mutataions/use-sign-up";
+import { useState } from "react";
 import { Link } from "react-router";
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate: signUp } = useSignUp();
+
+  const handleSignUpClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    signUp({
+      email,
+      password,
+    });
+  };
   return (
     <div className="flex flex-col gap-8">
       <div className="text-xl font-bold">회원가입</div>
       <div className="flex flex-col gap-2">
-        <Input className="py-6" type="email" placeholder="example@abc.com" />
-        <Input className="py-6" type="password" placeholder="password" />
+        <Input
+          className="py-6"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@abc.com"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+        />
       </div>
       <div>
-        <Button className="w-full">회원가입</Button>
+        <Button onClick={handleSignUpClick} className="w-full">
+          회원가입
+        </Button>
       </div>
       <div>
         <Link className="text-muted-foreground hover:underline" to={"/sign-in"}>
